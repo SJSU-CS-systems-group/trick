4ebba13b3c0dfd3ab149886335701b3b1f852514
jask1m <jkim94960@gmail.com>
2025-08-29 15:17:46 -0700

    Create KMP project setup

---
+++ composeApp/src/commonTest/kotlin/net/discdd/trick/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package net.discdd.trick
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
