fbe5438df91de60e959b8ad857550cd334105dd8
RonCodes88 <ronaldliyh@gmail.com>
2026-01-30 14:26:11 -0800

    Add message status enum for tracking message delivery state

---
+++ composeApp/src/commonMain/kotlin/net/discdd/trick/data/MessageStatus.kt
@@ -0,0 +1,12 @@
+package net.discdd.trick.data
+
+/**
+ * Status of a message in the system
+ */
+enum class MessageStatus {
+    PENDING,    // Message is queued but not yet sent
+    SENT,       // Message has been sent
+    DELIVERED,  // Message has been delivered to recipient
+    FAILED      // Message failed to send
+}
+
