c32c8cacd12e05643714722ef0f0a813c3eb4283
RonCodes88 <ronaldliyh@gmail.com>
2026-01-30 14:26:14 -0800

    Implement SQLite driver factory for Android platform

---
+++ composeApp/src/commonMain/kotlin/net/discdd/trick/data/DriverFactory.kt
@@ -0,0 +1,11 @@
+package net.discdd.trick.data
+
+import app.cash.sqldelight.db.SqlDriver
+
+/**
+ * Platform-specific factory for creating SQLite drivers
+ */
+expect class DriverFactory {
+    fun createDriver(): SqlDriver
+}
+
